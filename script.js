ffb29be656bf4baaaaee7f071e431844d6a1f12c
“Francislaino” <franx.barbosa@gmail.com>
2024-10-28 16:50:51 -0300

    adição dos arquivos index.html e script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function mostrarMensagem() {
+    console.log("Conexão feita com sucesso!");
+}
+
+mostrarMensagem();
+mostrarMensagem();
+mostrarMensagem();
+
+
+//outra opção seria usar o While e um contador
+
+// function mostrarMensagem() {
+//     let contador = 0;
+
+//     while (contador < 3) {
+//         console.log("Conexão feita com sucesso!");
+//         contador++;
+//     }
+// }
+
+// mostrarMensagem();
